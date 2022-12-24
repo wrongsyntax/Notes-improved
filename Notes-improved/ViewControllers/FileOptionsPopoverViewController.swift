10e9f24f12534c6dd7e03308d33b1583c29d0c00
Uzair Tariq <uzair0213@icloud.com>
2022-12-24 16:34:03 -0700

    rename optionspopover to fileoptionspopover

--- Notes-improved/ViewControllers/FileOptionsPopoverViewController.swift
+++ Notes-improved/ViewControllers/FileOptionsPopoverViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class OptionsPopoverViewController: UIViewController {
+class FileOptionsPopoverViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
